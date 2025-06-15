9476979693b8f7d6276038979947a892d62115f9
Aleksander Onysiak <114723280+Aleksander-Onysiak@users.noreply.github.com>
2025-06-15 21:20:20 +0200

    Fix css and js for mobile and desktop version

--- wp-content/themes/siteclient/src/js/main.js
+++ wp-content/themes/siteclient/src/js/main.js
@@ -1,28 +1,10 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const galleries = document.querySelectorAll('.gallery_container');
+const donateSection = document.body.querySelector('.js-qr-section');
+const donateButton = document.body.querySelector('.js-qr-section-button');
 
-  galleries.forEach(gallery => {
-    const mainView = gallery.querySelector('.gallery__main');
-    const mainImage = mainView.querySelector('.gallery__main-image');
-    const closeButton = mainView.querySelector('.close-button');
-    const items = gallery.querySelectorAll('.gallery__item');
 
-    items.forEach(item => {
-      item.addEventListener('click', () => {
-        const img = item.querySelector('img');
-        if (!img) return;
-
-        mainImage.src = img.dataset.full;
-        mainImage.alt = img.alt;
-
-        mainView.style.display = 'flex';
-        gallery.classList.add('gallery--active');
-      });
-    });
-
-    closeButton.addEventListener('click', () => {
-      mainView.style.display = 'none';
-      gallery.classList.remove('gallery--active');
-    });
+if (donateButton instanceof HTMLElement && donateSection instanceof HTMLElement) {
+  donateButton.addEventListener('click', () => {
+    console.log('test');
+    donateSection.classList.toggle('qr-section--hide');
   });
-});
+}
